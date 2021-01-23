7f88005d328bc6776f28c845c425add38b413519
Rob <rob@notyou.co.uk>
2021-01-23 07:57:35 +0000

    Initial Commit

---
+++ ColorUtility/ColorUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorUtilityApp.swift
+//  ColorUtility
+//
+//  Created by Rob on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
